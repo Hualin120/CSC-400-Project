79a8db836514214bf6913a122ece2d635462a9e0
Reynaldo Garcia <garciar17@southernct.edu>
2026-02-17 15:48:39 -0500

    Updated user password field to require a minimum of 8 characters.

--- forms.py
+++ forms.py
@@ -7,17 +7,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
-
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=150)])
-    email = EmailField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=30)])
+    email = EmailField('Email', validators=[DataRequired(), Email(), Length(max=50)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=128)])
     submit = SubmitField('Register')
 
-
 class LoginForm(FlaskForm):
-    email = EmailField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Email(), Length(max=50)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=1, max=128)])
     submit = SubmitField('Login')
-
 
